70258ea606d3e05e4a2cee8df59021ed84842984
Phil <97486086+Taichi-Ibi@users.noreply.github.com>
2025-10-19 10:10:08 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -65,7 +65,7 @@ export default function NotFound() {
             href="/home"
             className="rounded-lg bg-blue-500 px-5 py-2 text-sm font-medium text-white hover:bg-blue-600 dark:hover:bg-blue-400"
           >
-            トップページへ
+            ← 戻る
           </Link>
         </motion.div>
       </div>
