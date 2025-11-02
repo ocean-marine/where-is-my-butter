e703551e9a3edc719c1af1c7e31e8c8f45de8457
Phil <97486086+Taichi-Ibi@users.noreply.github.com>
2025-11-02 14:21:25 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -45,7 +45,7 @@ export default function NotFound() {
           initial={{ opacity: 0, y: 10 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ delay: 0.6, duration: 0.3 }}
-          className="flex gap-2"
+          className="flex gap-2 -mt-3"
         >
           <Link
             href="/home"
